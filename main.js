fc6806ce9b2e9ba3872aeeba09911d46cdf1fe38
ShomaHino <hino.pegasasu69.one@outlook.jp>
2023-07-30 18:05:45 +0900

    ポートフォリオサイトの開発を進めました

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-$('#page-link a[href*="#"]').click(function () {
-  //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
-  var elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-  var pos = $(elmHash).offset().top; //idの上部の距離を取得
-  $("body,html").animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-  return false;
-});
-
-$(".slider").slick({
-  autoplay: true, //自動的に動き出すか。初期値はfalse。
-  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-  speed: 500, //スライドのスピード。初期値は300。
-  slidesToShow: 3, //スライドを画面に3枚見せる
-  slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
-  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
-  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
-  centerMode: true, //要素を中央ぞろえにする
-  variableWidth: true, //幅の違う画像の高さを揃えて表示
-  dots: true, //下部ドットナビゲーションの表示
-});
+const navili = document.getElementsByClassName(".navi li");
+navili.scroll = function () {
+  navili.style.border = "solid";
+  navili.style.border - width;
+  ("thin");
+  behavior = smooth;
+};
